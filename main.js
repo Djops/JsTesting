49490483aa285203f423e9eb13f0072174d2eca4
Bessarab Evgany <Bevswf@gmail.com>
2020-07-13 18:48:00 +0300

    add new methods

--- main.js
+++ main.js
@@ -1,76 +1,80 @@
 
 
-let numberOfFilms;
 
-function start() {
-    while (numberOfFilms == "" || numberOfFilms == null || numberOfFilms == isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("сколько фильмов вы просмотрели ?");
-    }
-}
-
-start();
 
 
 let personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {
     },
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+
+    start: function () {
+        while (personalMovieDB.count == "" || personalMovieDB.count == null || personalMovieDB.count == isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("сколько фильмов вы просмотрели ?");
+        }
+    },
+
+    rememberMyFilims: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt("один из послдених просмотреных фильмов ?"),
+                b = prompt("на сколько оцените его ?");
+
+            if (a != "" && b != "" && a != null && b != null && a.length < 50 && b.length < 50) {
+                personalMovieDB.movies[a] = b;
+            } else {
+                i--;
+            }
+        }
+    },
+    writeYourGenres: function () {
+        for (let i = 1; i <= 3; i++) {
+            let answer = prompt(`выш любиммый жаннер под номером ${i}`);
+            if (answer != "" && answer != null) {
+                personalMovieDB.genres[i - 1] = answer;
+            } else {
+                i--;
+            }
+        }
+
+        personalMovieDB.genres.forEach(function (item, i) {
+            console.log(`Любимый жаннер: ${i + 1} : ${item} `);
+        });
+    },
+
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+
+    },
+
+    showMyDB: function (hiden) {
+        if (!hiden) {
+            console.log(personalMovieDB.privat);
+        }
+    },
 };
 
-
-function rememberMyFilims() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt("один из послдених просмотреных фильмов ?"),
-            b = prompt("на сколько оцените его ?");
-
-        if (a != "" && b != "" && a != null && b != null && a.length < 50 && b.length < 50) {
-            personalMovieDB.movies[a] = b;
-        } else {
-            i--;
-        }
-    }
-}
-
-rememberMyFilims();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("просмотрино давольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("вы любте смотреть кино");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("да вы киноман");
-    } else {
-        console.log("some erroe");
-    }
-
-}
-
-detectPersonalLevel();
+personalMovieDB.showMyDB()
+personalMovieDB.writeYourGenres()
 
 
-function showMyDB(hiden) {
-    if (!hiden) {
-        console.log(personalMovieDB.privat);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
 
 
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        let answer = prompt(`выш любиммый жаннер под номером ${i}`);
-        if (answer != "" && answer != null) {
-            personalMovieDB.genres[i - 1] = answer;
-        } else {
-            i--;
-        }
-    }
-}
-writeYourGenres();
+
+
+
+
+
+
+
+
+
+
 
 
